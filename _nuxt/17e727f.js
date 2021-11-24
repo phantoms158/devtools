b97af1e19d3033ee655d3498af1b41dba2f673ea
trongpq <trongpq@runsystem.net>
2021-11-24 14:21:38 +0700

    first commit

---
+++ _nuxt/17e727f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{969:function(t,e,n){"use strict";n.r(e);var c={layout:"dashboard"},l=n(11),component=Object(l.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("nuxt-child")}),[],!1,null,"5faeb82a",null);e.default=component.exports}}]);
